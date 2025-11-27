3a6bca3ae3f4fc6123c83906b2ab1331418892c8
Lovász Ottó <lovasz.otto@clarity.hu>
2025-11-27 09:42:12 +0100

    start.bat kiegészítés utf8

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 @echo off
+chcp 65001 >nul
+set PYTHONIOENCODING=utf-8
 REM =========================================
 REM  DUPLIKACIO ELLENORZO RENDSZER FUTTATAS
 REM =========================================
